ae3d1d2def1da202d0e20edbff2513dcf9d04d38
Barrett Pope <barrettpope94@gmail.com>
2020-12-15 16:02:36 -0500

    Scroll reveal animation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,3 +44,37 @@ function scrollActive() {
     }
   });
 }
+
+// Scroll Reveal Animation
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+// Home Section Scroll
+sr.reveal(".home-title", {});
+sr.reveal(".bounce", { delay: 200 });
+sr.reveal(".home-img", { origin: "right", delay: 400 });
+
+// About Section Scroll
+sr.reveal(".about-img", { delay: 500 });
+sr.reveal(".about-subtitle", { delay: 300 });
+sr.reveal(".about-profession", { delay: 400 });
+sr.reveal(".about-text", { delay: 500 });
+sr.reveal(".about-social-icon", { delay: 600, interval: 200 });
+
+// Skills Section Scroll
+sr.reveal(".skills-subtitle", {});
+sr.reveal(".skills-name", { distance: "20px", delay: 50, interval: 100 });
+sr.reveal(".skills-img", { delay: 400 });
+
+// Portfolio Section Scroll
+sr.reveal(".portfolio-img", { interval: 200 });
+
+// Contact Section Scroll
+sr.reveal(".contact-subtitle", {});
+sr.reveal(".contact-text", { interval: 200 });
+sr.reveal(".contact-input", { delay: 400 });
+sr.reveal(".contact-button", { delay: 600 });
