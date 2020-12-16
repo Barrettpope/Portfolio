da02da95d1c36e005ec8fc94108b1e2f1f944385
Barrett Pope <barrettpope94@gmail.com>
2020-12-16 15:30:49 -0500

    Contact form made functional with Formspree API

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,3 +78,48 @@ sr.reveal(".contact-subtitle", {});
 sr.reveal(".contact-text", { interval: 200 });
 sr.reveal(".contact-input", { delay: 400 });
 sr.reveal(".contact-button", { delay: 600 });
+
+window.addEventListener("DOMContentLoaded", function () {
+  // get the form elements defined in your form HTML above
+
+  var form = document.getElementById("my-form");
+  var status = document.getElementById("status");
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    status.classList.add("success");
+    status.innerHTML = "Thanks for reaching out! I'll be in touch soon.";
+  }
+
+  function error() {
+    status.innerHTML = "Oops! There was a problem.";
+    status.classList.add("error");
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
